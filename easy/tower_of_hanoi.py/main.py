532111104a7f124f7876c509cfadde5bace2fe9d
anamaya <sharma.011999@gmail.com>
2022-09-26 02:51:53 +0530

    Wrote the hanoi.py & written some unit tests for tower_of_hanoi

--- easy/tower_of_hanoi.py/main.py
+++ easy/tower_of_hanoi.py/main.py
@@ -4,6 +4,31 @@ from hanoi import Hanoi
 
 class Test(unittest.TestCase):
     """ Tower of Hanoi Unit Tests """
+    def empty_test(self):
+        """ Empty Test """
+        self.assertEqual(Hanoi(), 0)
+
+    def negative_test(self):
+        """ Negative Test """
+        self.assertEqual(Hanoi(-1), 0)
+
     def test_hanoi(self):
         """ Test Tower of Hanoi """
         self.assertEqual(Hanoi(3), 7)
+
+    def test_hanoi2(self):
+        """ Test Tower of Hanoi """
+        self.assertEqual(Hanoi(4), 15)
+
+    def test_hanoi3(self):
+        """ Test Tower of Hanoi """
+        self.assertEqual(Hanoi(5), 31)
+
+    def test_hanoi4(self):
+        """ Test Tower of Hanoi """
+        self.assertEqual(Hanoi(100), 2**100 - 1)
+
+    
+
+if __name__ == "__main__":
+    unittest.main()
